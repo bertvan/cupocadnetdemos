de61cba92b005fa584cd58582c15651e7c1988f0
vanpeteghem.bert <vanpeteghem.bert@a7e4f2c6-1272-11de-a1e7-776c1f3a7222>
2009-10-11 15:52:33 +0000

    Added Sample Collection Solution, with PaletteJig Sample

---
+++ SampleCollection/SampleCollection/PaletteJig/PLineJig.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace SampleCollection.PaletteJig
+{
+    /// <summary>
+    /// Courtesy of Through the Interface: http://through-the-interface.typepad.com/through_the_interface/2006/11/controlling_int_1.html <br/>
+    /// Included to demonstrate a problem that I had with this Jig
+    /// </summary>
+    public class PlineJig : EntityJig
+    {
+        // Maintain a list of vertices...
+        // Not strictly necessary, as these will be stored in the
+        // polyline, but will not adversely impact performance
+        Point3dCollection m_pts;
+        // Use a separate variable for the most recent point...
+        // Again, not strictly necessary, but easier to reference
+        Point3d m_tempPoint;
+        Plane m_plane;
+
+        public PlineJig(Matrix3d ucs)
+            : base(new Polyline())
+        {
+            // Create a point collection to store our vertices
+            m_pts = new Point3dCollection();
+
+            // Create a temporary plane, to help with calcs
+            Point3d origin = new Point3d(0, 0, 0);
+            Vector3d normal = new Vector3d(0, 0, 1);
+            normal = normal.TransformBy(ucs);
+            m_plane = new Plane(origin, normal);
+
+            // Create polyline, set defaults, add dummy vertex
+            Polyline pline = Entity as Polyline;
+            pline.SetDatabaseDefaults();
+            pline.Normal = normal;
+            pline.AddVertexAt(0, new Point2d(0, 0), 0, 0, 0);
+        }
+
+        protected override SamplerStatus Sampler(JigPrompts prompts)
+        {
+            JigPromptPointOptions jigOpts =
+              new JigPromptPointOptions();
+            jigOpts.UserInputControls =
+              (UserInputControls.Accept3dCoordinates |
+              UserInputControls.NullResponseAccepted |
+              UserInputControls.NoNegativeResponseAccepted
+              );
+
+            if (m_pts.Count == 0)
+            {
+                // For the first vertex, just ask for the point
+                jigOpts.Message =
+                  "\nStart point of polyline: ";
+            }
+            else if (m_pts.Count > 0)
+            {
+                // For subsequent vertices, use a base point
+                jigOpts.BasePoint = m_pts[m_pts.Count - 1];
+                jigOpts.UseBasePoint = true;
+                jigOpts.Message = "\nPolyline vertex: ";
+            }
+            else // should never happen
+                return SamplerStatus.Cancel;
+
+            // Get the point itself
+            PromptPointResult res =
+              prompts.AcquirePoint(jigOpts);
+
+            // Check if it has changed or not
+            // (reduces flicker)
+            if (m_tempPoint == res.Value)
+            {
+                return SamplerStatus.NoChange;
+            }
+            else if (res.Status == PromptStatus.OK)
+            {
+                m_tempPoint = res.Value;
+                return SamplerStatus.OK;
+            }
+            return SamplerStatus.Cancel;
+        }
+
+        protected override bool Update()
+        {
+            // Update the dummy vertex to be our
+            // 3D point projected onto our plane
+            Polyline pline = Entity as Polyline;
+            pline.SetPointAt(
+              pline.NumberOfVertices - 1,
+              m_tempPoint.Convert2d(m_plane)
+            );
+            return true;
+        }
+
+        public Entity GetEntity()
+        {
+            return Entity;
+        }
+
+        public void AddLatestVertex()
+        {
+            // Add the latest selected point to
+            // our internal list...
+            // This point will already be in the
+            // most recently added pline vertex
+            m_pts.Add(m_tempPoint);
+            Polyline pline = Entity as Polyline;
+            // Create a new dummy vertex...
+            // can have any initial value
+            pline.AddVertexAt(
+              pline.NumberOfVertices,
+              new Point2d(0, 0),
+              0, 0, 0
+            );
+        }
+
+        public void RemoveLastVertex()
+        {
+            // Let's remove our dummy vertex
+            Polyline pline = Entity as Polyline;
+            pline.RemoveVertexAt(m_pts.Count);
+        }
+    }
+}
---
+++ SampleCollection/SampleCollection/PaletteJig/PaletteUserControl.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SampleCollection.PaletteJig
+{
+    partial class PaletteUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.promptPolyDirectlyButton = new System.Windows.Forms.Button();
+            this.promptPolylineViaCommand = new System.Windows.Forms.Button();
+            this.numberOfPointsLabel = new System.Windows.Forms.Label();
+            this.numberOfPointsTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // promptPolyDirectlyButton
+            // 
+            this.promptPolyDirectlyButton.Location = new System.Drawing.Point(3, 3);
+            this.promptPolyDirectlyButton.Name = "promptPolyDirectlyButton";
+            this.promptPolyDirectlyButton.Size = new System.Drawing.Size(166, 23);
+            this.promptPolyDirectlyButton.TabIndex = 0;
+            this.promptPolyDirectlyButton.Text = "Prompt polyline directly";
+            this.promptPolyDirectlyButton.UseVisualStyleBackColor = true;
+            this.promptPolyDirectlyButton.Click += new System.EventHandler(this.promptPolyDirectlyButton_Click);
+            // 
+            // promptPolylineViaCommand
+            // 
+            this.promptPolylineViaCommand.Location = new System.Drawing.Point(3, 32);
+            this.promptPolylineViaCommand.Name = "promptPolylineViaCommand";
+            this.promptPolylineViaCommand.Size = new System.Drawing.Size(166, 23);
+            this.promptPolylineViaCommand.TabIndex = 0;
+            this.promptPolylineViaCommand.Text = "Prompt polyline via command";
+            this.promptPolylineViaCommand.UseVisualStyleBackColor = true;
+            this.promptPolylineViaCommand.Click += new System.EventHandler(this.promptPolylineViaCommand_Click);
+            // 
+            // numberOfPointsLabel
+            // 
+            this.numberOfPointsLabel.AutoSize = true;
+            this.numberOfPointsLabel.Location = new System.Drawing.Point(3, 87);
+            this.numberOfPointsLabel.Name = "numberOfPointsLabel";
+            this.numberOfPointsLabel.Size = new System.Drawing.Size(140, 13);
+            this.numberOfPointsLabel.TabIndex = 1;
+            this.numberOfPointsLabel.Text = "Number of points in Polyline:";
+            // 
+            // numberOfPointsTextbox
+            // 
+            this.numberOfPointsTextbox.Enabled = false;
+            this.numberOfPointsTextbox.Location = new System.Drawing.Point(3, 103);
+            this.numberOfPointsTextbox.Name = "numberOfPointsTextbox";
+            this.numberOfPointsTextbox.Size = new System.Drawing.Size(100, 20);
+            this.numberOfPointsTextbox.TabIndex = 2;
+            // 
+            // PaletteUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.numberOfPointsTextbox);
+            this.Controls.Add(this.numberOfPointsLabel);
+            this.Controls.Add(this.promptPolylineViaCommand);
+            this.Controls.Add(this.promptPolyDirectlyButton);
+            this.Name = "PaletteUserControl";
+            this.Size = new System.Drawing.Size(180, 192);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button promptPolyDirectlyButton;
+        private System.Windows.Forms.Button promptPolylineViaCommand;
+        private System.Windows.Forms.Label numberOfPointsLabel;
+        private System.Windows.Forms.TextBox numberOfPointsTextbox;
+    }
+}
